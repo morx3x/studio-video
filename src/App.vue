fad2afe793f55293d2561f4b643ec15b2c109e88
KaiKeima <keimakun2011@gmail.com>
2018-09-22 03:54:48 +0900

    レイアウトした

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#app
+  Editor(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from "./components/Editor.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Editor
   }
-}
+};
 </script>
 
-<style>
+<style lang="stylus">
+html
+body
+  overflow hidden
+*
+  padding 0
+  margin 0
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,38 @@
+
+<template lang="pug">
+.editor
+  header
+  .cuts
+  main
+  footer
+</template>
+
+<script>
+export default {
+  name: "Editor",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.editor
+  display grid
+  width 100vw
+  height 100vh
+  grid-template-areas "header header" "cuts main" "cuts footer"
+  grid-template-columns 240px 1fr
+  grid-template-rows 60px 1fr 180px
+  header
+    grid-area header
+    box-shadow 0 0 10px rgba(0,0,0,0.1)
+    background #fff
+  .cuts
+    grid-area cuts
+  main
+    grid-area main
+    background rgba(0,0,0,0.05)
+  footer
+    grid-area footer
+</style>
